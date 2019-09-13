46b565ffd5b20deb3b4daad8c4139f5fa0041bcf
iemeka <mekstines@gmail.com>
2019-09-13 09:02:37 +0100

    hmm

--- expense/routes.py
+++ expense/routes.py
@@ -213,9 +213,14 @@ def expense_routes(app):
         all_titles = get_all_titles()
 
         response = None
-        if cost == "" or int(cost) == False:
+        if cost == "" :
             failure ={"data":None,
                 "error":"pls type in the appropriate value"
+            }
+            response = jsonify(failure)
+        elif cost.isdigit() == False:
+            failure ={"data":None,
+                "error":"cost should have a numerical value"
             }
             response = jsonify(failure)
         else:
